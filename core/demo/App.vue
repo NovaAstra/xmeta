e754b770ad8c3a0cd49ddfb660b87ea35690ff6e
August.Gao <August.Gao@mspbots.ai>
2024-03-21 19:11:31 +0800

    初始化

--- core/demo/App.vue
+++ core/demo/App.vue
@@ -3,7 +3,14 @@
 </template>
 
 <script lang="ts" setup>
-import { string } from '@xmeta/budd/src/index'
+import { string } from '@xmeta/budd'
+import {string as s} from 'yup'
 
 string()
+
+try{
+    console.log(await s().validate(2,{strict:true}))
+}catch(e){
+    console.log(e)
+}
 </script>
