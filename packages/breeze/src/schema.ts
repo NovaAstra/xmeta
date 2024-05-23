a5e8dde364cd193d23dd28688a2541316c400b53
GaoYu <NovaAstra@outlook.com>
2024-05-23 21:10:39 +0800

    重置

--- packages/breeze/src/schema.ts
+++ packages/breeze/src/schema.ts
@@ -1,17 +1,1 @@
-export enum SchemaTypeKind {
-  BreezeString = "BreezeString",
-}
-
-export interface SchemaImple {
-  readonly $type: SchemaTypeKind;
-}
-
-export abstract class Schema implements SchemaImple {
-  public abstract $type: SchemaTypeKind;
-
-  protected clone() {
-    const next = Object.create(Object.getPrototypeOf(this));
-
-    return next as this;
-  }
-}
+export abstract class Schema {}
